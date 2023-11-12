aa5c24e90c27743e3c0b057d541b445bb03ceca8
April Phuong <aprilphuong@gmail.com>
2023-11-12 12:32:00 -0800

    added basic navigation and styling

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import "../styles/Navbar.css"
+import { Button } from 'react-bootstrap'
+
+
+// Navbar.js
+export default function Navbar() {
+    return (
+        <nav className="navigation">
+          <a href="/" className="logo-section">
+              <div className="job-tracker-title">JobTrekker</div>
+          </a>
+
+        </nav>
+      
+    );
+  }
