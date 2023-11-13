6bafef3bebec3cf9a59f1e4f8633c70af1777941
April Phuong <aprilphuong@gmail.com>
2023-11-12 16:31:22 -0800

    added more styling and links to sidebar nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,14 +3,19 @@ import { Button } from 'react-bootstrap'
 
 
 // Navbar.js
-export default function Navbar() {
+function Navbar() {
     return (
-        <nav className="navigation">
-          <a href="/" className="logo-section">
-              <div className="job-tracker-title">JobTrekker</div>
-          </a>
 
+        <nav className="sidebar">
+            <a href="/" className="logo-section">
+              <div className="job-tracker-title">JobTrekker</div>
+            </a>
+            <a href="/applications">Applications</a>
+            <a href="/contacts">Contacts</a>
+            <a href="/skills">Skills</a>
         </nav>
       
     );
   }
+
+  export default Navbar;
