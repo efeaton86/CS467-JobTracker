352da623c27166e350c349b1722d28654769ae63
April Phuong <aprilphuong@gmail.com>
2023-11-12 16:33:08 -0800

    moved Navbar from Applications.js into App.js and added more styling

--- client/src/pages/Applications.js
+++ client/src/pages/Applications.js
@@ -1,12 +1,10 @@
 
-import Navbar from "../components/Navbar";
 import JobAppTable from "../components/JobAppTable"
 
 function Applications() {
   
   return (
     <div className="Applications">
-        <Navbar />   
         <JobAppTable />
     </div>
   );
