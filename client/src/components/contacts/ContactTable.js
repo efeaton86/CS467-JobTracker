d962f5f7e76023d63daeafc23c155654382801a2
efeaton86 <50210677+efeaton86@users.noreply.github.com>
2023-11-19 12:50:24 +0100

    removed some deprecated comments

--- client/src/components/contacts/ContactTable.js
+++ client/src/components/contacts/ContactTable.js
@@ -1,57 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bulma/css/bulma.min.css';
 
 import "../../styles/Applications.css";
 import Table from "react-bootstrap/Table";
 import ContactTableRow from "./ContactTableRow";
 
-function ContactTable({contacts, onUpdateContact, onDeleteContact}) {
-
-  // const [editRowId, setEditRowId] = useState(null);
-  // const [editedData, setEditedData] = useState({});
-  // const [isModalOpen, setModalOpen] = useState(false);
-
-  // const openModal = () => {
-  //   setModalOpen(true);
-  // };
-
-  // const closeModal = () => {
-  //   setModalOpen(false);
-  // };
-
-  // const handleEditClick = (rowId) => {
-  //   setEditRowId(rowId);
-  // };
-  //
-  // const handleCancelClick = () => {
-  //   setEditRowId(null);
-  // };
+function ContactTable({contacts, onDeleteContact}) {
 
   const handleDeleteClick = (rowId) => {
     onDeleteContact(rowId)
   }
-
-//  const handleUpdateClick = async (rowId) => {
-//   try {
-//     const response = await fetch(`/api/contacts/${rowId}`, {
-//       method: 'PUT',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(editedData),
-//     });
-//     if (response.ok) {
-//       onUpdateContact(rowId, editedData);
-//       setEditRowId(null);
-//       setEditedData({})
-//     } else {
-//       // TODO: refactor this to display fields with an error
-//       console.error('Error updating contact.');
-//     }
-//   } catch (error) {
-//     console.error('Error updating contact:', error);
-//   }
-// };
 
   return (
     <>
